8789ce2994c57d0f999a7c478d21c0d9dc0806cc
Andrew S <>
2025-06-09 14:20:58 +0300

    feat: implement Logger class

--- src/Logger.java
+++ src/Logger.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Logger {
+    protected int num = 1;
+    private static Logger logger;
+
+    public void log(String msg) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+        System.out.printf("[%s %d] %s%n", timestamp, num, msg);
+        num++;
+    }
+
+    public static Logger getInstance() {
+        if (logger == null) {
+            logger = new Logger();
+        }
+        return logger;
+    }
 }
